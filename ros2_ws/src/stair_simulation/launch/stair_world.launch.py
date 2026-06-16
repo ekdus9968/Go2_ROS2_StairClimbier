821cf92b2be9cfd1b0d0e13930f3659218e6163f
ekdus9968 <kan.seyoung1018@gmail.com>
2026-06-16 18:06:25 -0400

    PHASE 5b setup

--- ros2_ws/src/stair_simulation/launch/stair_world.launch.py
+++ ros2_ws/src/stair_simulation/launch/stair_world.launch.py
@@ -1,25 +1,30 @@
+"""Launch Gazebo with patient stairs world."""
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import PathJoinSubstitution
+
 
 def generate_launch_description():
+    # World file path
+    pkg_dir = get_package_share_directory('stair_simulation')
+    world_file = os.path.join(pkg_dir, 'worlds', 'patient_stairs.world')
+    
+    # Gazebo launch
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('gazebo_ros'),
+                'launch', 'gazebo.launch.py'
+            )
+        ]),
+        launch_arguments={
+            'world': world_file,
+            'verbose': 'true',
+        }.items()
+    )
+    
     return LaunchDescription([
-        # Gazebo
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([
-                FindPackageShare('gazebo_ros'),
-                '/launch/gazebo.launch.py'
-            ]),
-            launch_arguments={
-                'world': PathJoinSubstitution([
-                    FindPackageShare('stair_simulation'),
-                    'worlds',
-                    'patient_stairs.world'
-                ]),
-            }.items()
-        ),
-        # rl_sar gazebo (already exist : rl_sar/src/rl_sar/launch/gazebo.launch.py)
-        # ...
+        gazebo_launch,
     ])
--- ros2_ws/src/stair_simulation/setup.py
+++ ros2_ws/src/stair_simulation/setup.py
@@ -1,27 +1,31 @@
-from setuptools import find_packages, setup
+import os
+from glob import glob
+from setuptools import setup
 
 package_name = 'stair_simulation'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # World file
+        (os.path.join('share', package_name, 'worlds'),
+            glob('worlds/*.world')),
+        # Launch file
+        (os.path.join('share', package_name, 'launch'),
+            glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='jen',
     maintainer_email='kan.seyoung1018@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    description='Gazebo simulation with patient stairs',
+    license='Apache-2.0',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
         ],
